45e3fc8a99a4035c0b99f496eb9330da124dd8c1
VJarreta <738737@unizar.es>
2021-01-11 20:05:03 +0100

    RC4 done with a 100 times loop

--- src/main/java/RC4.java
+++ src/main/java/RC4.java
@@ -1,8 +1,12 @@
 import javax.crypto.Cipher;
+import javax.crypto.ShortBufferException;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
 import java.security.Provider;
 import java.security.Security;
 import java.sql.Timestamp;
@@ -21,34 +25,12 @@ import static org.junit.Assert.assertNotEquals;
 
 public class RC4 {
 
-    public static byte[] rc4encrypt(byte[] msg, byte[] key) {
-
-        RC4Engine inCipher = new RC4Engine();
-        inCipher.init(true, new KeyParameter(key));
-        byte[] outcipher = new byte[msg.length];
-        inCipher.processBytes(msg, 0, msg.length, outcipher, 0);
-
-        return outcipher;
-    }
-
-    public static byte[] rc4decrypt(byte[] msg, byte[] key) {
-
-        RC4Engine inCipher = new RC4Engine();
-        inCipher.init(false, new KeyParameter(key));
-        byte[] outcipher = new byte[msg.length];
-
-        inCipher.processBytes(msg,0,msg.length, outcipher, 0);
-        return outcipher;
-    }
-
-
-    public static void main(String[] args) {
+    public static void bucle() {
+        Double time20k = 0.0, time100k = 0.0, time200k = 0.0;
+        int nTimes = 100;
         try{
             Security.addProvider(new BouncyCastleProvider());
             String file ="src/main/resources/files/20k";
-            String result = null;
-            RC4Engine rc4Engine = new RC4Engine();
-            RC4Engine decrypter = new RC4Engine();
 
             DataInputStream reader = new DataInputStream(new FileInputStream(file));
             byte[] input20k = new byte[reader.available()];
@@ -69,127 +51,123 @@ public class RC4 {
             byte[] keyBytes = new byte[reader.available()];
             reader.read(keyBytes);
 
-            //Outputs:
-            byte[] output20k = new byte[input20k.length];
-            byte[] output100k = new byte[input100k.length];
-            byte[] output200k = new byte[input200k.length];
-
             // Define Key + Cipher
             SecretKeySpec key = new SecretKeySpec(keyBytes, "RC4");
 
             Cipher cipher = Cipher.getInstance("RC4",BouncyCastleProvider.PROVIDER_NAME);
 
-            //20kB:
+            int j = 10;
+            while(j>0){
+                byte[] cipherText = new byte[input20k.length];
+                cipher.init(Cipher.ENCRYPT_MODE, key);
+                int ctLength = cipher.update(input20k, 0, input20k.length, cipherText, 0);
+                ctLength += cipher.doFinal(cipherText, ctLength);
 
-            // encryption pass
+                // decryption pass
+                byte[] plainText = new byte[ctLength];
+                cipher.init(Cipher.DECRYPT_MODE, key);
+                int ptLength = cipher.update(cipherText, 0, ctLength, plainText, 0);
+                ptLength += cipher.doFinal(plainText, ptLength);
+                j--;
+            }
 
-            byte[] cipherText = new byte[input20k.length];
+            for(int i=0; i<nTimes; i++){
+                //20kB:
 
-            cipher.init(Cipher.ENCRYPT_MODE, key);
+                // encryption pass
 
-            Timestamp t20k1 = new Timestamp(System.nanoTime());
+                byte[] cipherText = new byte[input20k.length];
+                cipher.init(Cipher.ENCRYPT_MODE, key);
+                Timestamp t20k1 = new Timestamp(System.nanoTime());
+                int ctLength = cipher.update(input20k, 0, input20k.length, cipherText, 0);
+                ctLength += cipher.doFinal(cipherText, ctLength);
+                Timestamp t20k2 = new Timestamp(System.nanoTime());
 
-            int ctLength = cipher.update(input20k, 0, input20k.length, cipherText, 0);
+                time20k += (t20k2.getTime() - t20k1.getTime())/1000;
 
-            ctLength += cipher.doFinal(cipherText, ctLength);
+                // decryption pass
+                byte[] plainText = new byte[ctLength];
+                cipher.init(Cipher.DECRYPT_MODE, key);
+                int ptLength = cipher.update(cipherText, 0, ctLength, plainText, 0);
+                ptLength += cipher.doFinal(plainText, ptLength);
 
-            Timestamp t20k2 = new Timestamp(System.nanoTime());
+                String input = new String(input20k);
+                String output = new String(plainText);
 
-
-            System.out.println("RC4 With 20KB: " + (t20k2.getTime() - t20k1.getTime())/1000 + (" us"));
-
-            // decryption pass
-
-            byte[] plainText = new byte[ctLength];
-
-            cipher.init(Cipher.DECRYPT_MODE, key);
-
-            int ptLength = cipher.update(cipherText, 0, ctLength, plainText, 0);
-
-            ptLength += cipher.doFinal(plainText, ptLength);
-
-            String input = new String(input20k);
-            String output = new String(plainText);
-
-            assertEquals(input, output);
-
-
-            //100kB:
-
-
-            // encryption pass
-
-            cipherText = new byte[input100k.length];
-
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-
-            Timestamp t100k1 = new Timestamp(System.nanoTime());
-
-            ctLength = cipher.update(input100k, 0, input100k.length, cipherText, 0);
-
-            ctLength += cipher.doFinal(cipherText, ctLength);
-
-            Timestamp t100k2 = new Timestamp(System.nanoTime());
+                assertEquals(input, output);
 
 
-            System.out.println("RC4 With 100KB: " + (t100k2.getTime() - t100k1.getTime())/1000 + (" us"));
+                //100kB:
 
-            // decryption pass
+                // encryption pass
 
-            plainText = new byte[ctLength];
+                cipherText = new byte[input100k.length];
+                cipher.init(Cipher.ENCRYPT_MODE, key);
+                Timestamp t100k1 = new Timestamp(System.nanoTime());
+                ctLength = cipher.update(input100k, 0, input100k.length, cipherText, 0);
+                ctLength += cipher.doFinal(cipherText, ctLength);
+                Timestamp t100k2 = new Timestamp(System.nanoTime());
 
-            cipher.init(Cipher.DECRYPT_MODE, key);
+                time100k += (t100k2.getTime() - t100k1.getTime())/1000;
 
-            ptLength = cipher.update(cipherText, 0, ctLength, plainText, 0);
+                // decryption pass
 
-            ptLength += cipher.doFinal(plainText, ptLength);
+                plainText = new byte[ctLength];
+                cipher.init(Cipher.DECRYPT_MODE, key);
+                ptLength = cipher.update(cipherText, 0, ctLength, plainText, 0);
+                ptLength += cipher.doFinal(plainText, ptLength);
+                input = new String(input100k);
+                output = new String(plainText);
 
-            input = new String(input100k);
-            output = new String(plainText);
-
-            assertEquals(input, output);
-
-
-
-            //200kB:
-
-
-            // encryption pass
-
-            cipherText = new byte[input200k.length];
-
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-
-            Timestamp t200k1 = new Timestamp(System.nanoTime());
-
-            ctLength = cipher.update(input200k, 0, input200k.length, cipherText, 0);
-
-            ctLength += cipher.doFinal(cipherText, ctLength);
-
-            Timestamp t200k2 = new Timestamp(System.nanoTime());
+                assertEquals(input, output);
 
 
-            System.out.println("RC4 With 200KB: " + (t200k2.getTime() - t200k1.getTime())/1000 + (" us"));
+                //200kB:
 
-            // decryption pass
 
-            plainText = new byte[ctLength];
+                // encryption pass
 
-            cipher.init(Cipher.DECRYPT_MODE, key);
+                cipherText = new byte[input200k.length];
+                cipher.init(Cipher.ENCRYPT_MODE, key);
+                Timestamp t200k1 = new Timestamp(System.nanoTime());
+                ctLength = cipher.update(input200k, 0, input200k.length, cipherText, 0);
+                ctLength += cipher.doFinal(cipherText, ctLength);
+                Timestamp t200k2 = new Timestamp(System.nanoTime());
 
-            ptLength = cipher.update(cipherText, 0, ctLength, plainText, 0);
 
-            ptLength += cipher.doFinal(plainText, ptLength);
+                time200k += (t200k2.getTime() - t200k1.getTime())/1000;
 
-            input = new String(input200k);
-            output = new String(plainText);
+                // decryption pass
 
-            assertEquals(input, output);
+                plainText = new byte[ctLength];
+                cipher.init(Cipher.DECRYPT_MODE, key);
+                ptLength = cipher.update(cipherText, 0, ctLength, plainText, 0);
+                ptLength += cipher.doFinal(plainText, ptLength);
+                input = new String(input200k);
+                output = new String(plainText);
+
+                assertEquals(input, output);
+            }
+
+            time20k /= nTimes;
+            time100k /= nTimes;
+            time200k /= nTimes;
 
             System.out.println("Algorithm: " + cipher.getAlgorithm() + ", Provider: " + cipher.getProvider().getName());
+
+            System.out.println("Time with 20k: " + time20k);
+            System.out.println("Time with 100k: " + time100k);
+            System.out.println("Time with 200k: " + time200k);
 
         }catch(Exception e){
             e.printStackTrace();
         }
+
+
+    }
+
+
+    public static void main(String[] args) {
+        bucle();
     }
 }
